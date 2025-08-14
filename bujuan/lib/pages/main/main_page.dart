1fad2016cd9e718e27eaa2e5626a8ca121551009
lololzx <3183371740@qq.com>
2025-08-15 00:05:06 +0800

    Add files via upload

---
+++ bujuan/lib/pages/main/main_page.dart
@@ -0,0 +1,231 @@
+import 'package:bujuan_music/common/bujuan_music_handler.dart';
+import 'package:bujuan_music/common/values/app_images.dart';
+import 'package:bujuan_music/pages/main/menu_page.dart';
+import 'package:bujuan_music/pages/main/provider.dart';
+import 'package:bujuan_music/pages/play/play_page.dart';
+import 'package:bujuan_music/router/app_router.dart';
+import 'package:bujuan_music/widgets/backdrop.dart';
+import 'package:bujuan_music/widgets/cache_image.dart';
+import 'package:bujuan_music/widgets/rive_player.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:get_it/get_it.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hugeicons/hugeicons.dart';
+import '../../utils/adaptive_screen_utils.dart';
+import '../../widgets/slide.dart';
+import 'package:rive_native/rive_native.dart' as rive;
+
+class MainPage extends StatelessWidget {
+  final Widget child;
+
+  const MainPage({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    bool desktop = medium(context) || expanded(context);
+    return Scaffold(
+      body: desktop ? DesktopView(child: child) : MobileView(child: child),
+    );
+  }
+}
+
+class MobileView extends StatelessWidget {
+  final Widget child;
+
+  const MobileView({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    var of = MediaQuery.of(context);
+    double minHeightBox = 45.w + (of.padding.bottom == 0 ? 20.w : of.padding.bottom);
+    double maxHeightBox = of.size.height - of.padding.top - 5.w;
+    return ZoomDrawer(
+        controller: GetIt.I<ZoomDrawerController>(),
+        reverseDuration: Duration(milliseconds: 1000),
+        menuScreen: MenuPage(),
+        menuScreenWidth: MediaQuery.of(context).size.width,
+        mainScreenScale: .18,
+        angle: -8,
+        slideWidth: 200.w,
+        dragOffset: 200.w,
+        shadowLayer1Color: Colors.grey.withAlpha(20),
+        shadowLayer2Color: Colors.grey.withAlpha(30),
+        showShadow: true,
+        mainScreenTapClose: true,
+        menuScreenTapClose: true,
+        mainScreen: Consumer(builder: (context, ref, c) {
+          return SlidingBox(
+            controller: GetIt.I<BoxController>(),
+            collapsed: true,
+            minHeight: minHeightBox,
+            maxHeight: maxHeightBox,
+            color: Theme.of(context).scaffoldBackgroundColor,
+            style: BoxStyle.sheet,
+            draggableIconVisible: false,
+            draggableIconBackColor: Colors.white,
+            onBoxOpen: () {},
+            onBoxSlide: (value) =>
+                ref.read(boxPanelDetailDataProvider.notifier).updatePanelDetail(value),
+            backdrop: Backdrop(
+              moving: false,
+              color: Theme.of(context).scaffoldBackgroundColor,
+              body: child,
+            ),
+            bodyBuilder: (s, _) => GestureDetector(
+              child: PlayPage(),
+              onHorizontalDragUpdate: (e) {},
+            ),
+          );
+        }));
+  }
+}
+
+class DesktopView extends StatelessWidget {
+  final Widget child;
+
+  const DesktopView({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    var of = MediaQuery.of(context);
+    return SizedBox(
+      width: of.size.width,
+      height: of.size.height,
+      child: Stack(
+        children: [
+          Consumer(builder: (context, ref, child) {
+            var watch = ref.watch(backgroundModeNotifierProvider);
+            return RivePlayer(
+              asset: watch,
+              fit: rive.Fit.cover,
+              autoBind: true,
+            );
+          }),
+          Column(
+            children: [
+              SizedBox(height: 10.w),
+              _buildSearch(),
+              SizedBox(height: 10.w),
+              Expanded(
+                  child: Row(
+                    children: [
+                      SizedBox(width: 10.w),
+                      MenuPage(),
+                      SizedBox(width: 10.w),
+                      Expanded(child: BackdropView(child: child)),
+                      SizedBox(width: 10.w),
+                    ],
+                  )),
+              SizedBox(height: 10.w),
+              _buildBottomBar(context),
+              SizedBox(height: 10.w),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBottomBar(BuildContext context) {
+    return GestureDetector(
+      child: BackdropView(
+        height: 52.w,
+        padding: EdgeInsets.symmetric(horizontal: 15.w),
+        child: Wrap(
+          runAlignment: WrapAlignment.center,
+          children: [
+            IconButton(
+                onPressed: () => BujuanMusicHandler().skipToPrevious(),
+                icon: Icon(
+                  HugeIcons.strokeRoundedPrevious,
+                  size: 22.sp,
+                )),
+            SizedBox(width: 10.w),
+            Consumer(builder: (context, ref, child) {
+              var playbackState = ref.watch(playbackStateProvider).value;
+              return IconButton(
+                  onPressed: () => BujuanMusicHandler().playOrPause(),
+                  icon: Icon(
+                    (playbackState?.playing ?? false)
+                        ? HugeIcons.strokeRoundedPause
+                        : HugeIcons.strokeRoundedPlay,
+                    size: 22.sp,
+                  ));
+            }),
+            SizedBox(width: 10.w),
+            IconButton(
+                onPressed: () => BujuanMusicHandler().skipToNext(),
+                icon: Icon(
+                  HugeIcons.strokeRoundedNext,
+                  size: 22.sp,
+                )),
+            SizedBox(width: 15.w),
+            Container(
+              height: 40.w,
+              padding: EdgeInsets.symmetric(horizontal: 8.w),
+              decoration: BoxDecoration(
+                  color: Colors.grey.withAlpha(20), borderRadius: BorderRadius.circular(30.w)),
+              child: Consumer(builder: (context, ref, child) {
+                var media = ref.watch(mediaItemProvider).value;
+                return Wrap(
+                  runAlignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  children: [
+                    CachedImage(
+                      imageUrl: media?.artUri.toString() ?? '',
+                      width: 30.w,
+                      height: 30.w,
+                      borderRadius: 23.w,
+                    ),
+                    SizedBox(width: 20.w),
+                    Text(
+                      media?.title ?? 'Bujuan',
+                      style: TextStyle(fontSize: 14.sp),
+                    ),
+                    SizedBox(width: 15.w),
+                    IconButton(onPressed: () {}, icon: Icon(HugeIcons.strokeRoundedFavourite)),
+                  ],
+                );
+              }),
+            ),
+            SizedBox(width: 10.w),
+            IconButton(onPressed: () {}, icon: Icon(HugeIcons.strokeRoundedVolumeHigh))
+          ],
+        ),
+      ),
+      onTap: () {
+        var path = GoRouter.of(context).state.path;
+        if (path == AppRouter.play) {
+          context.pop();
+          return;
+        }
+        context.push(AppRouter.play);
+      },
+    );
+  }
+
+  Widget _buildSearch() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Image.asset(AppImages.logo, width: 45.w, height: 45.w),
+        SizedBox(width: 10.w),
+        BackdropView(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          height: 46.w,
+          width: 460.w,
+          child: Row(
+            children: [
+              Icon(HugeIcons.strokeRoundedAiSearch02),
+              SizedBox(width: 15.w),
+              Text('Search Any...')
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
---
+++ bujuan/lib/pages/play/play_page.dart
@@ -0,0 +1,395 @@
+import 'dart:ui';
+
+import 'package:bujuan_music/common/bujuan_music_handler.dart';
+import 'package:bujuan_music/utils/time_utils.dart';
+import 'package:bujuan_music/widgets/cache_image.dart';
+import 'package:bujuan_music/widgets/panel.dart';
+import 'package:bujuan_music/widgets/slide.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get_it/get_it.dart';
+import 'package:hugeicons/hugeicons.dart';
+import 'package:palette_generator/palette_generator.dart';
+import '../../utils/color_utils.dart';
+import '../../widgets/curved_progress_bar.dart';
+import '../main/provider.dart';
+
+class PlayPage extends StatelessWidget {
+  const PlayPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final minHeightBox = 45.w + (mediaQuery.padding.bottom == 0 ? 20.w : mediaQuery.padding.bottom);
+    final maxHeightBox = mediaQuery.size.height - mediaQuery.padding.top - 5.w;
+
+    return SizedBox(
+      width: mediaQuery.size.width,
+      height: maxHeightBox,
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: [
+          Positioned.fill(
+            child: Consumer(builder: (context, ref, child) {
+              final color = ref.watch(mediaColorProvider).maybeWhen(
+                    data: (c) => c.dominantColor?.color ?? Colors.transparent,
+                    orElse: () => Colors.transparent,
+                  );
+
+              return AnimatedGradientBackground(
+                startColor: Theme.of(context).scaffoldBackgroundColor,
+                endColor: ColorUtils.lightenColor(color, .5).withAlpha(100),
+              );
+            }),
+          ),
+          const _MusicControlsSection(),
+          Consumer(
+              builder: (context, ref, child) => Positioned(
+                    top: -minHeightBox * ref.watch(boxPanelDetailDataProvider),
+                    child: GestureDetector(
+                      child: const SongInfoBar(),
+                      onTap: () => GetIt.I<BoxController>().openBox(),
+                    ),
+                  )),
+        ],
+      ),
+    );
+  }
+}
+
+class AnimatedGradientBackground extends StatelessWidget {
+  final Color startColor;
+  final Color endColor;
+
+  const AnimatedGradientBackground({super.key, required this.startColor, required this.endColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<Color?>(
+      tween: ColorTween(begin: startColor, end: endColor),
+      duration: const Duration(milliseconds: 500),
+      builder: (context, color, _) => Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [startColor, color ?? Colors.transparent],
+            begin: Alignment.topLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CoverBackground extends ConsumerWidget {
+  const CoverBackground({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var watch = ref.watch(mediaItemProvider);
+    return Stack(
+      children: [
+        CachedImage(
+          imageUrl: watch.value?.artUri.toString() ?? '',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
+        ),
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10), // sigma 越大模糊越强
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class SongInfoBar extends ConsumerWidget {
+  const SongInfoBar({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mediaItem = ref.watch(mediaItemProvider).value;
+
+    return Container(
+      color: Colors.transparent,
+      width: MediaQuery.of(context).size.width,
+      child: Row(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 30.w),
+            child: SizedBox(
+              height: 60.w,
+              child: Row(
+                children: [
+                  CachedImage(
+                    imageUrl: mediaItem?.artUri.toString() ?? '',
+                    width: 40.w,
+                    height: 40.w,
+                    borderRadius: 20.w,
+                  ),
+                  SizedBox(width: 10.w),
+                  Text(mediaItem?.title ?? 'Bujuan',
+                      style: TextStyle(fontSize: 16.sp, fontWeight: FontWeight.w500)),
+                ],
+              ),
+            ),
+          ),
+          const Spacer(),
+          Padding(
+            padding: EdgeInsets.only(right: 10.w),
+            child: IconButton(
+              onPressed: () => BujuanMusicHandler().playOrPause(),
+              icon: Icon(
+                ref.watch(playbackStateProvider).value?.playing == true
+                    ? HugeIcons.strokeRoundedPause
+                    : HugeIcons.strokeRoundedPlay,
+                size: 24.w,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MusicControlsSection extends ConsumerWidget {
+  const _MusicControlsSection();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final mediaQuery = MediaQuery.of(context);
+    final minHeightBox =
+        45.w + (mediaQuery.padding.bottom == 0 ? 20.w : mediaQuery.padding.bottom) * 2;
+    final mediaItem = ref.watch(mediaItemProvider).value;
+
+    return Column(
+      children: [
+        AnimatedContainer(
+          duration: Duration.zero,
+          height: minHeightBox * (1 - ref.watch(boxPanelDetailDataProvider)),
+        ),
+        SizedBox(
+          height: 35.w,
+          child: Transform(
+            transform: Matrix4.translationValues(0, -15, 0),
+            child: Icon(Icons.remove_rounded, color: Colors.grey.withAlpha(120), size: 62.w),
+          ),
+        ),
+        SizedBox(height: 10.w),
+        Column(
+          children: [
+            CachedImage(
+              imageUrl: mediaItem?.artUri.toString() ?? '',
+              width: 290.w,
+              height: 290.w,
+              borderRadius: 25.w,
+              pWidth: 600,
+              pHeight: 600,
+            ),
+            SizedBox(height: 20.w),
+            Text(mediaItem?.title ?? '',
+                style: TextStyle(fontSize: 22.sp, fontWeight: FontWeight.w500)),
+            SizedBox(height: 3.w),
+            Text(mediaItem?.artist ?? '',
+                style: TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400, color: Colors.grey)),
+          ],
+        ),
+        Expanded(child: _ProgressBarWithTime()),
+        const _PlaybackControls(),
+        SizedBox(height: 80.w),
+      ],
+    );
+  }
+}
+
+class Sleep extends ConsumerWidget {
+  const Sleep({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final color = ref.watch(mediaColorProvider).maybeWhen(
+          data: (c) => c.dominantColor!,
+          orElse: () => PaletteColor(Colors.white, 1),
+        );
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 20.w, vertical: 30.w),
+      padding: EdgeInsets.symmetric(vertical: 12.w, horizontal: 25.w),
+      decoration: BoxDecoration(
+          color: ColorUtils.lightenColor(color.color, 0.7),
+          borderRadius: BorderRadius.circular(30.w)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                HugeIcons.strokeRoundedAlarmClock,
+                size: 16.sp,
+              ),
+              Text('21:36')
+            ],
+          ),
+          Text('1.2x'),
+          Icon(
+            HugeIcons.strokeRoundedMenu04,
+            size: 16.sp,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _ProgressBarWithTime extends ConsumerWidget {
+  const _ProgressBarWithTime();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final color = ref.watch(mediaColorProvider).maybeWhen(
+          data: (c) => c.dominantColor!,
+          orElse: () => PaletteColor(Colors.green, 1),
+        );
+    final position = ref.watch(playbackStateProvider).value?.updatePosition.inSeconds ?? 0;
+    final duration = ref.watch(mediaItemProvider).value?.duration?.inSeconds ?? 0;
+    final progress = duration > 0 ? position / duration : 0.0;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20.w),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IgnoreDraggableWidget(
+            child: TweenAnimationBuilder<double>(
+              tween: Tween(begin: 0, end: progress),
+              duration: const Duration(milliseconds: 80),
+              builder: (context, value, _) => CurvedProgressBar(
+                progress: value,
+                progressColor: ColorUtils.lightenColor(color.color, 0.6),
+                activeProgressColor: ColorUtils.lightenColor(color.color, 0.2),
+                onProgressChange: (value) =>
+                    BujuanMusicHandler().seek(Duration(seconds: (duration * value).toInt())),
+              ),
+            ),
+          ),
+          const SizedBox(height: 4),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(TimeUtils.formatDuration(position), style: _timeTextStyle),
+              Text(TimeUtils.formatDuration(duration), style: _timeTextStyle),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlaybackControls extends StatelessWidget {
+  const _PlaybackControls();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(width: 15.w),
+        const _ControlButton(image: HugeIcons.strokeRoundedFavourite),
+        SizedBox(width: 15.w),
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _ControlButton(
+                image: HugeIcons.strokeRoundedPrevious,
+                onTap: () => BujuanMusicHandler().skipToPrevious(),
+              ),
+              const _PlayPauseButton(),
+              _ControlButton(
+                image: HugeIcons.strokeRoundedNext,
+                onTap: () => BujuanMusicHandler().skipToNext(),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(width: 15.w),
+        Consumer(builder: (context, ref, child) {
+          var loopMode = ref.watch(loopModeNotifierProvider);
+          return _ControlButton(
+            image: loopMode == LoopMode.one
+                ? HugeIcons.strokeRoundedRepeatOne02
+                : loopMode == LoopMode.playlist
+                    ? HugeIcons.strokeRoundedRepeat
+                    : HugeIcons.strokeRoundedShuffle,
+            onTap: () {
+              ref.read(loopModeNotifierProvider.notifier).changeMode();
+            },
+          );
+        }),
+        SizedBox(width: 15.w),
+      ],
+    );
+  }
+}
+
+class _ControlButton extends StatelessWidget {
+  final IconData image;
+  final VoidCallback? onTap;
+
+  const _ControlButton({required this.image, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        image,
+        size: 24.sp,
+        color: Colors.black,
+      ),
+      onPressed: onTap,
+    );
+  }
+}
+
+class _PlayPauseButton extends ConsumerWidget {
+  const _PlayPauseButton();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final color = ref.watch(mediaColorProvider).maybeWhen(
+          data: (c) => c.dominantColor!,
+          orElse: () => PaletteColor(Colors.white, 1),
+        );
+
+    return Container(
+      padding: EdgeInsets.all(3.w),
+      decoration: BoxDecoration(
+        color: color.color,
+        borderRadius: BorderRadius.circular(50.w),
+      ),
+      child: IconButton(
+        onPressed: () => BujuanMusicHandler().playOrPause(),
+        icon: Icon(
+          ref.watch(playbackStateProvider).value?.playing == true
+              ? HugeIcons.strokeRoundedPause
+              : HugeIcons.strokeRoundedPlay,
+          color: color.titleTextColor,
+          size: 24.sp,
+        ),
+      ),
+    );
+  }
+}
+
+const _timeTextStyle = TextStyle(
+  fontSize: 14,
+  color: Colors.grey,
+  fontWeight: FontWeight.w600,
+);
